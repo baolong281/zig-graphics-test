f5ff0e7a761608408e3bfa098c44d94c0ef55d64
dylan h <baolong281@gmail.com>
2024-08-04 14:53:44 -0500

    shader errors

--- src/main.zig
+++ src/main.zig
@@ -2,29 +2,12 @@ const std = @import("std");
 const gl = @import("gl");
 const glfw = @import("glfw");
 const math = std.math;
+const shaders = @import("shaders.zig");
 
 const glfw_log = std.log.scoped(.glfw);
 const gl_log = std.log.scoped(.gl);
 
 var gl_procs: gl.ProcTable = undefined;
-
-const vertex_shader_source =
-    \\#version 330 core
-    \\layout (location = 0) in vec3 aPos;
-    \\void main()
-    \\{
-    \\    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
-    \\}
-;
-
-const fragment_shader_source =
-    \\#version 330 core
-    \\out vec4 FragColor;
-    \\void main()
-    \\{
-    \\    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
-    \\}
-;
 
 fn logGLFWError(error_code: c_int, description: [*:0]const u8) callconv(.C) void {
     glfw_log.err("{}: {s}\n", .{ error_code, description });
@@ -69,25 +52,6 @@ pub fn main() !void {
     // -- setting up viewport --
     gl.Viewport(0, 0, 800, 800);
 
-    // -- create and compile shaders --
-    const vertex_shader = gl.CreateShader(gl.VERTEX_SHADER);
-    gl.ShaderSource(vertex_shader, 1, (&vertex_shader_source.ptr)[0..1], null);
-    gl.CompileShader(vertex_shader);
-
-    const fragment_shader = gl.CreateShader(gl.FRAGMENT_SHADER);
-    gl.ShaderSource(fragment_shader, 1, (&fragment_shader_source.ptr)[0..1], null);
-    gl.CompileShader(fragment_shader);
-
-    // -- create and link shader program --
-    const shader_program = gl.CreateProgram();
-    gl.AttachShader(shader_program, vertex_shader);
-    gl.AttachShader(shader_program, fragment_shader);
-    defer gl.DeleteShader(vertex_shader);
-    defer gl.DeleteShader(fragment_shader);
-    defer gl.DeleteProgram(shader_program);
-
-    gl.LinkProgram(shader_program);
-
     // define the triangles vertices
     const vertices = [_]f32{
         -0.5, -0.5 * math.sqrt(3) / 3.0, 0.0,
@@ -104,6 +68,8 @@ pub fn main() !void {
         3, 4, 2,
         5, 4, 1,
     };
+
+    const shader_program = try shaders.init_shaders();
 
     // -- allocate and bind vertex buffer in gpu memory --
     // VAO must be bound before binding the VBO
---
+++ src/shaders.zig
@@ -0,0 +1,67 @@
+const std = @import("std");
+const gl = @import("gl");
+
+const vertex_shader_source =
+    \\#version 330 core
+    \\layout (location = 0) in vec3 aPos;
+    \\void main()
+    \\{
+    \\    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    \\}
+;
+
+const fragment_shader_source =
+    \\#version 330 core
+    \\out vec4 FragColor;
+    \\void main()
+    \\{
+    \\    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    \\}
+;
+
+const ShaderError = error {
+    VertexShaderFailedToCompile,
+    FragmentShaderFailedToCompile,
+};
+
+pub fn init_shaders() !c_uint {
+    var info_log: [1024:0]u8 = undefined;
+
+    // -- create and compile shaders --
+    const vertex_shader = gl.CreateShader(gl.VERTEX_SHADER);
+    gl.ShaderSource(vertex_shader, 1, (&vertex_shader_source.ptr)[0..1], null);
+    gl.CompileShader(vertex_shader);
+    defer gl.DeleteShader(vertex_shader);
+
+    var status: c_int = undefined;
+    gl.GetShaderiv(vertex_shader, gl.COMPILE_STATUS, &status);
+    if (status == gl.FALSE) {
+        gl.GetShaderInfoLog(vertex_shader, info_log.len, null, &info_log);
+        std.debug.print("Vertex shader compile log:\n{s}\n", .{info_log});
+        return error.VertexShaderFailedToCompile;
+    }
+
+    const fragment_shader = gl.CreateShader(gl.FRAGMENT_SHADER);
+    gl.ShaderSource(fragment_shader, 1, (&fragment_shader_source.ptr)[0..1], null);
+    gl.CompileShader(fragment_shader);
+
+    status = undefined;
+    gl.GetShaderiv(fragment_shader, gl.COMPILE_STATUS, &status);
+    if (status == gl.FALSE) {
+        gl.GetShaderInfoLog(fragment_shader, info_log.len, null, &info_log);
+        std.debug.print("Fragment shader compile log:\n{s}\n", .{info_log});
+        return error.FragmentShaderFailedToCompile;
+    }
+    defer gl.DeleteShader(fragment_shader);
+
+    // -- create and link shader program --
+    const shader_program = gl.CreateProgram();
+    gl.AttachShader(shader_program, vertex_shader);
+    gl.AttachShader(shader_program, fragment_shader);
+    defer gl.DetachShader(shader_program, vertex_shader);
+    defer gl.DetachShader(shader_program, fragment_shader);
+
+    gl.LinkProgram(shader_program);
+
+    return  shader_program;
+}
